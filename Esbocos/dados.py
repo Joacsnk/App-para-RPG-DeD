5f2eda3c16345cbf6da3d4ac46c2c673d203d45a
Joacsnk <Joacsnk@gmail.com>
2025-01-07 10:43:12 -0300

    Teste dos dados
    Feito a funcionalidade (em teste) de rolagem de dados

--- Esbocos/dados.py
+++ Esbocos/dados.py
@@ -1,6 +1,31 @@
+from random import randint
 def escolha_de_dados():
     import interface
-    tipo_de_dado = int(input('\nD4 (1)\nD6 (2)\nD8 (3)\nD10 (4)\nD20 (5)\nD100 (6)\nSAIR (7)\n\nESCOLHA O TIPO DO DADO: '))
-    match tipo_de_dado:
-        case 7:
+    pagina = True
+    while pagina == True:
+        print('---Dados---\n')
+        tipo_de_dado = int(input('\nESCOLHA A QUANTIDADE DE LADOS (OU ''0'' PARA SAIR): '))
+        if tipo_de_dado == 0:
+            print('\n' * 130)
             interface.inicio()
+        elif tipo_de_dado > 0:
+            x = True
+            while x == True:
+                print('\n' * 130)
+                print('O DADO CAIU NO NÚMERO: {}'.format(queda_dados(tipo_de_dado)))
+                rolagem = int(input('ROLAR NOVAMENTE(1)\nTROCAR NÚMERO DOS DADOS(2)\nSAIR(3): '))
+                match rolagem:
+                    case 1:
+                        x = True
+                    case 2:
+                        print('\n' * 130)
+                        x = False
+                    case 3:
+                        x = False
+                        pagina = False
+                        print('\n' * 130)
+                        interface.inicio()
+        else:
+            print('ERRO - - - Valor incorreto\n')
+def queda_dados(lados_dado):
+    return randint(1, lados_dado)

04fa0c2860e6ff4c278503207dae5b9555a39e5e
Joacsnk <Joacsnk@gmail.com>
2025-01-15 22:23:37 -0300

    Testes no código original

--- Esbocos/interface.py
+++ Esbocos/interface.py
@@ -1,12 +1,14 @@
 import dados
 def inicio ():
     print('---------------D&D---------------\n')
-    escolhainterfaceinicial = int(input('DADOS (1)\nSAIR (2)\n\nESCOLHA SUA OPÇÃO: '))
+    escolhainterfaceinicial = int(input('DADOS (1)\nSAIR (3)\n\nESCOLHA SUA OPÇÃO: '))
     match escolhainterfaceinicial:
         case 1:
             print('\n' * 130)
             dados.escolha_de_dados()
         case 2:
+            print('\n' * 130)
+        case 3:
             exit()
 if __name__ == "__main__":
     inicio()
